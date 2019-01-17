a02199c66f6282a69c37e709b417399a986a95a9
Simon <slyguggi@gmail.com>
2019-01-18 00:17:21 +0100

    added dolly basic dolly zoom

--- 4300Game5/Assets/Scripts/CameraController.cs
+++ 4300Game5/Assets/Scripts/CameraController.cs
@@ -4,27 +4,4 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-	[SerializeField, Range(0, 180)] private float fieldOfView = 45.0f;
-	[SerializeField] private float width = 1.0f;
-	private Camera myCamera;
-	private Vector3 originPosition;
-	private float originFOV;
-
-	private void Start()
-	{
-		myCamera = GetComponent<Camera>();
-		originFOV = myCamera.fieldOfView;
-		originPosition = transform.position;
-	}
-
-	private void Update()
-	{
-		myCamera.fieldOfView = fieldOfView;
-		transform.position = originPosition + Vector3.forward * width/(2*Mathf.Tan(fieldOfView/2));
-
-		//width that'll remains unchanged
-		//distance the camera has to move
-		//FOV fov
-		//distance = width/2tan(FOV/2)
-	}
 }

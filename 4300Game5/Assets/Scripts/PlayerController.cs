33e2a5dea94248decf19ed78c539651d0caaa44a
Simon <slyguggi@gmail.com>
2019-01-19 14:21:16 +0100

    added destabilizing player

--- 4300Game5/Assets/Scripts/PlayerController.cs
+++ 4300Game5/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
 	[SerializeField] private float speedForward = 5.0f;
 	[SerializeField] private float speedLateral = 5.0f;
 	[SerializeField] private float timeStopMoving = 2.0f;
+	[SerializeField] private float minForceRandom = 0.1f;
+	[SerializeField] private float maxForceRandom = 2.0f;
 	private GameObject ground;
 	private float horizontalInput = 0.0f;
 	private bool isAlive = true;
@@ -18,13 +20,22 @@ public class PlayerController : MonoBehaviour
 	private void Start()
 	{
 		myRigidBody = GetComponent<Rigidbody>();
+		if (minForceRandom>maxForceRandom)
+		{
+			minForceRandom = maxForceRandom;
+		}
+		else if (maxForceRandom < minForceRandom)
+		{
+			maxForceRandom = minForceRandom;
+		}
 	}
 
 	private void FixedUpdate()
 	{
 		if (canMove)
 		{
-			myRigidBody.velocity = new Vector3(speedLateral*horizontalInput, myRigidBody.velocity.y, speedForward);
+			myRigidBody.velocity = new Vector3(speedLateral * horizontalInput, myRigidBody.velocity.y,
+				speedForward * Mathf.Sign(transform.forward.z));
 		}
 	}
 
@@ -34,6 +45,7 @@ public class PlayerController : MonoBehaviour
 		{
 			horizontalInput = Input.GetAxis("Horizontal");
 			//transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speedLateral, 0, Time.deltaTime * speedForward);
+			Destabilize();
 		}
 	}
 
@@ -45,13 +57,23 @@ public class PlayerController : MonoBehaviour
 		}
 	}
 
-	/*private void OnCollisionExit(Collision other)
+	void Destabilize()
+	{
+		float pos = transform.position.x;
+		if (pos.CompareTo(0) == 0)
+		{
+			pos *= Mathf.Pow(-1.0f, Random.Range(0, 1));
+		}
+		myRigidBody.AddForce(Vector3.right * Mathf.Sign(pos) * Random.Range(minForceRandom,maxForceRandom));
+	}
+
+	private void OnCollisionExit(Collision other)
 	{
 		if (other.gameObject.CompareTag("Ground"))
 		{
 			canMove = false;
 		}
-	}*/
+	}
 
 	public void Die()
 	{
@@ -66,11 +88,12 @@ public class PlayerController : MonoBehaviour
 
 	public IEnumerator StopMoving()
 	{
+		float initSpeed = speedForward;
 		float timer = 0.0f;
 		while (timer < timeStopMoving)
 		{
 			timer += Time.deltaTime;
-			speedForward = Mathf.Lerp(speedForward, 0.0f, timer / timeStopMoving);
+			speedForward = Mathf.Lerp(initSpeed, 0.0f, timer / timeStopMoving);
 			yield return null;
 		}
 
